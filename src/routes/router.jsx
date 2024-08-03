e2e898dd2220bd9da399dbc5f5c9f713ea54e4f7
Molaei <molaie.web.developer@gmail.com>
2024-08-03 01:58:39 +0100

    Create a component for application route

---
+++ src/routes/router.jsx
@@ -0,0 +1,11 @@
+import { useReducer } from "react";
+
+const ApplicationRouter = () => {
+    const routers = useReducer([
+        // {path: "/", element: ""}
+    ])
+    
+    return routers;
+}
+ 
+export default ApplicationRouter;
