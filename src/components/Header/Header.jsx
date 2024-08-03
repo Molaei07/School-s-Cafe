24e93db6de4f2099c283ad6e74ab918fde532156
Molaei <molaie.web.developer@gmail.com>
2024-08-03 01:55:35 +0100

    Create a component for Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import HeaderMenu from "./header-components/HeaderMenu"
+import HeaderButtons from "./header-components/HeaderButtons"
+import ApplicationName from "../ApplicationName/applicationName"
+
+const Header = () => {
+    return (
+        <div className="w-full h-20 relative z-10 text-white flex items-center justify-between px-40 font-Vazir text-xl">
+            <ApplicationName />
+            <HeaderMenu />
+            <HeaderButtons />
+        </div>
+    )
+}
+
+export default Header
