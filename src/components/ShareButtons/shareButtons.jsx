29610529b5372793d7d128fab27297e6c047cb26
Molaei <molaie.web.developer@gmail.com>
2024-08-03 01:58:12 +0100

    Create a component for all page buttons

---
+++ src/components/ShareButtons/shareButtons.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom"
+
+// eslint-disable-next-line react/prop-types
+const ShareButtons = ({ name, backgroundColor, borderRadius, textColor, address, width, textSize }) => {
+    return (
+        <Link
+            style={{borderRadius: borderRadius + "px", width: width + "px", color: textColor, fontSize: textSize + "px"}}
+            to={address}
+            className={`
+                ${backgroundColor ? "bg-darkYellow" : "bg-transparent"}
+                px-6 py-2 font-semibold border-2 border-darkYellow buttonsShadow
+            `}
+        >{name}</Link>
+    )
+}
+
+export default ShareButtons
