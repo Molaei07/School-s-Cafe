8b50d883d93a77a058223fb483419b9c1e51a846
Molaei <molaie.web.developer@gmail.com>
2024-08-05 00:35:54 +0100

    Add categories data

---
+++ src/data/Categories/categories.js
@@ -0,0 +1,15 @@
+import Fruit from "../../assets/images/Fruit.jfif"
+import Breakfast from "../../assets/images/Dairy and breakfast.jfif"
+import Beverages from "../../assets/images/Beverages.jpg"
+import Sandwich from "../../assets/images/Sandwich.jpg"
+import Sweets from "../../assets/images/sweets.jpg"
+import Snacks from "../../assets/images/snacks.jpg"
+
+export const categoryList = [
+    {id: 1, name: "لبنیات و صبحانه", picture: Breakfast },
+    {id: 2, name: "میوه", picture: Fruit },
+    {id: 3, name: "نوشیدنی", picture: Beverages },
+    {id: 4, name: "ساندویچ", picture: Sandwich },
+    {id: 5, name: "شیرینی", picture: Sweets },
+    {id: 6, name: "تنقلات", picture: Snacks },
+]
