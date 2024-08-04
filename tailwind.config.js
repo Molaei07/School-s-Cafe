8aaa6d6105cbc0c6d90ead5e9e320f807d24262c
Molaei <molaie.web.developer@gmail.com>
2024-08-05 00:32:56 +0100

    Add new animation and new font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      keyframes: {
+        SpinningAndAppearing: {
+          "0%": { transform: "rotate(-300deg) scale(0)" },
+          "100%": { transform: "rotate(-15deg) scale(1)" },
+        },
+      },
+      animation: {
+        SpinningAndAppearing: "SpinningAndAppearing 1s forwards",
+      },
       colors: {
-        "darkBlue": "#011F26",
-        "darkYellow": "#FFC224",
-        "darkCyan": "#025E73"
-      }
+        darkBlue: "#011F26",
+        darkYellow: "#FFC224",
+        darkCyan: "#025E73",
+      },
     },
     fontFamily: {
-      "Vazir": '"Vazir"'
-    }
+      SahelBold: '"SahelBold"',
+      SahelMedium: '"SahelMedium"',
+    },
   },
   plugins: [],
-}
+};
