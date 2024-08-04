4e22b9c2737e4e6b0c88588da2ed54cfe9536082
Molaei <molaie.web.developer@gmail.com>
2024-08-05 00:35:33 +0100

    Create router

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,11 +1,20 @@
-import { useReducer } from "react";
+import Layout from "../components/Layout/Main";
+import Landing from "../components/Landing";
+import { Navigate, useRoutes } from "react-router-dom";
 
 const ApplicationRouter = () => {
-    const routers = useReducer([
-        // {path: "/", element: ""}
+    const routers = useRoutes([
+        {
+            path: "/",
+            element: <Layout />,
+            children: [
+                { path: "/", element: <Landing /> },
+                { path: "/home", element: <Navigate to={"/"} /> },
+            ]
+        }
     ])
-    
+
     return routers;
 }
- 
+
 export default ApplicationRouter;
